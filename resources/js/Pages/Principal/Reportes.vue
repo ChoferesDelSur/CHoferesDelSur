7b7dec00e3854be3473b547e543d3cee97c3f451
Ernesto <ernestoalonsomartinezmartinez@gmail.com>
2024-06-12 15:45:48 -0600

    Creacion de componentes para reporte

--- resources/js/Pages/Principal/Reportes.vue
+++ resources/js/Pages/Principal/Reportes.vue
@@ -5,13 +5,10 @@ import Swal from 'sweetalert2';
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 import { ref, reactive } from 'vue';
-import * as XLSX from 'xlsx';
-import axios from 'axios';
+import ReportesEntrada from '../../Components/Reportes/ReportesEntrada.vue';
 
 // Cargar fuentes personalizadas
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
-const entradas = ref([]);
 
 const props = defineProps({
     message: { String, default: '' },
@@ -26,284 +23,19 @@ const props = defineProps({
     },
 });
 
-const form = reactive({
-    unidad: null, // Puedes inicializarlo con algún valor predeterminado si lo deseas
-    operador: null
-});
-
-const fetchEntradas = async (idUnidad, periodo) => {
-    let url = '';
-    if (periodo.tipo === 'semana') {
-        url = route('reportes.entradasSemana', { idUnidad: idUnidad, semana: periodo.valor });
-    } else if (periodo.tipo === 'mes' || typeof periodo === 'number') {
-        url = route('reportes.entradasMes', { idUnidad: idUnidad, mes: periodo.valor });
-    } else if (periodo.tipo === 'anio') {
-        url = route('reportes.entradasAnio', { idUnidad: idUnidad, anio: periodo.valor });
-    }
-
-    try {
-        const response = await axios.get(url);
-        entradas.value = response.data;
-    } catch (error) {
-        /* console.error(error); */
-        Swal.fire({
-            title: 'Error',
-            text: 'No se pudieron obtener los datos',
-            icon: 'error',
-            confirmButtonText: 'OK'
-        });
-    }
-};
-
-const generarArchivo = async (reporte, formato, idUnidad, periodoSeleccionado) => {
-    let periodo = { tipo: reporte.periodoSeleccionado, valor: '' };
-    if (reporte.periodoSeleccionado === 'semana') {
-        periodo.valor = semanaSeleccionada;
-    } else if (reporte.periodoSeleccionado === 'mes') {
-        periodo.valor = mesSeleccionado;
-    } else if (reporte.periodoSeleccionado === 'anio') {
-        periodo.valor = anioSeleccionado;
-    }
-
-    try {
-        await fetchEntradas(idUnidad, periodo);
-        if (reporte.titulo === 'Entradas') {
-            if (formato === 'pdf') {
-                generarPDF(reporte.titulo, periodo); // Pasa el objeto periodo completo
-            } else if (formato === 'excel') {
-                generarExcel(reporte.titulo, periodo);
-            } else if (formato === 'imprimir') {
-                imprimirReporte(reporte.titulo, periodo);
-            }
-        } else {
-            Swal.fire({
-                title: `Generar el reporte "${reporte.titulo}" en ${formato}`,
-                text: 'Lógica para generar este tipo de reporte aquí',
-                icon: 'info',
-                confirmButtonText: 'OK'
-            });
-        }
-    } catch (error) {
-        console.error(error);
-        Swal.fire({
-            title: 'Error',
-            text: 'No se pudieron obtener los datos',
-            icon: 'error',
-            confirmButtonText: 'OK'
-        });
-    }
-};
-
-const generarPDF = (tipo, periodoSeleccionado) => {
-    let periodoTexto;
-    // Ajustar el texto del periodo según el tipo
-    if (periodoSeleccionado.tipo === 'semana') {
-        periodoTexto = `Semana ${periodoSeleccionado.valor}`;
-    } else if (periodoSeleccionado.tipo === 'mes') {
-        periodoTexto = months[periodoSeleccionado.valor - 1];
-    } else if (periodoSeleccionado.tipo === 'anio') {
-        periodoTexto = periodoSeleccionado.valor; // Asumiendo que `anioSeleccionado` ya está en `periodoSeleccionado.valor`
-    } else {
-        periodoTexto = periodoSeleccionado.valor; // Default case
-    }
-
-    const bodyContent = entradas.value.map(entry => {
-        const ruta = entry.unidad?.ruta ? entry.unidad.ruta.nombreRuta : 'N/A';
-        const fecha = entry.created_at ? new Date(entry.created_at).toLocaleDateString() : 'N/A';
-        const numeroUnidad = entry.unidad?.numeroUnidad || 'N/A';
-        const directivo = entry.unidad?.directivo ? `${entry.unidad.directivo.nombre_completo}` : 'N/A';
-        const horaEntrada = entry.horaEntrada ? entry.horaEntrada.substring(0, 5) : 'N/A';
-        const tipoEntrada = entry.tipoEntrada;
-        const extremo = entry.extremo || 'N/A';
-        const operador = entry.operador ? `${entry.operador.nombre_completo}` : 'N/A';
-
-        return [ruta, fecha, numeroUnidad, directivo, horaEntrada, tipoEntrada, extremo, operador];
-    });
-
-    // Construye el nombre del archivo con el tipo de reporte y el período
-    const nombreArchivo = `${tipo}-${periodoTexto}.pdf`;
-
-    const docDefinition = {
-        content: [
-            { text: `Reporte de ${tipo} - Período: ${periodoTexto}`, style: 'header' },
-            {
-                table: {
-                    headerRows: 1,
-                    widths: ['*', 'auto', 'auto', 'auto', '*', 'auto', 'auto', 'auto'], // Ajustar según el número de columnas
-                    body: [
-                        [
-                            { text: 'Ruta', style: 'tableHeader', alignment: 'center' },
-                            { text: 'Fecha', style: 'tableHeader', alignment: 'center' },
-                            { text: 'Numero Unidad', style: 'tableHeader', alignment: 'center' },
-                            { text: 'Socio/Prestador', style: 'tableHeader', alignment: 'center' },
-                            { text: 'Hora Entrada', style: 'tableHeader', alignment: 'center' },
-                            { text: 'Tipo Entrada', style: 'tableHeader', alignment: 'center' },
-                            { text: 'Extremo', style: 'tableHeader', alignment: 'center' },
-                            { text: 'Operador', style: 'tableHeader', alignment: 'center' }
-                        ],
-                        ...bodyContent
-                    ]
-                }
-            }
-        ],
-        styles: {
-            header: { fontSize: 16, bold: true },
-            tableHeader: { bold: true }
-        },
-        pageOrientation: 'landscape'
-    };
-
-    pdfMake.createPdf(docDefinition).download(nombreArchivo);
-};
-
-const generarExcel = (tipo, periodoSeleccionado) => {
-    let periodoTexto;
-
-    // Ajustar el texto del periodo según el tipo
-    if (periodoSeleccionado.tipo === 'semana') {
-        periodoTexto = `Semana ${periodoSeleccionado.valor}`;
-    } else if (periodoSeleccionado.tipo === 'mes') {
-        periodoTexto = months[periodoSeleccionado.valor - 1];
-    } else if (periodoSeleccionado.tipo === 'anio') {
-        periodoTexto = periodoSeleccionado.valor; // Asumiendo que `anioSeleccionado` ya está en `periodoSeleccionado.valor`
-    } else {
-        periodoTexto = periodoSeleccionado.valor; // Default case
-    }
-
-    const nombreArchivo = `${tipo}-${periodoTexto}.xlsx`;
-    // Crear datos para el archivo Excel
-    const data = [['Ruta', 'Fecha', 'Numero Unidad', 'Socio/Prestador', 'Hora Entrada', 'Tipo Entrada', 'Extremo', 'Operador']];
-    entradas.value.forEach(entry => {
-        const ruta = entry.unidad?.ruta ? entry.unidad.ruta.nombreRuta : 'N/A';
-        const fecha = entry.created_at ? new Date(entry.created_at).toLocaleDateString() : 'N/A';
-        const numeroUnidad = entry.unidad?.numeroUnidad || 'N/A';
-        const directivo = entry.unidad?.directivo ? `${entry.unidad.directivo.nombre_completo}` : 'N/A';
-        const horaEntrada = entry.horaEntrada ? entry.horaEntrada.substring(0, 5) : 'N/A';
-        const tipoEntrada = entry.tipoEntrada;
-        const extremo = entry.extremo || 'N/A';
-        const operador = entry.operador ? `${entry.operador.nombre_completo}` : 'N/A';
-        data.push([ruta, fecha, numeroUnidad, directivo, horaEntrada, tipoEntrada, extremo, operador]);
-    });
-
-    // Crear libro de Excel
-    const workbook = XLSX.utils.book_new();
-    const worksheet = XLSX.utils.aoa_to_sheet(data);
-
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Reporte');
-    // Guardar el archivo Excel
-    XLSX.writeFile(workbook, nombreArchivo);
-};
-
-const reportes = [
-    { titulo: 'Entradas', periodo: 'semana', periodoSeleccionado: 'semana' },
-    { titulo: 'Cortes con regreso', periodo: 'semana', periodoSeleccionado: 'semana' },
-    { titulo: 'Cortes sin regreso', periodo: 'semana', periodoSeleccionado: 'semana' }, // Ejemplo: Podrías establecerlo en 'dia' por defecto
-    { titulo: 'Retardos', periodo: 'semana', periodoSeleccionado: 'semana' } // Ejemplo: Podrías establecerlo en 'dia' por defecto
-];
-
-const formatos = [
-    { tipo: 'pdf', texto: 'Generar PDF', clase: 'bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded', icono: 'fa-solid fa-file-pdf' },
-    { tipo: 'excel', texto: 'Generar Excel', clase: 'bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded', icono: 'fa-solid fa-file-excel' },
-];
-
-// Definir semanas
-const weeks = Array.from({ length: 52 }, (_, i) => i + 1);
-
-// Definir meses
-const months = [
-    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
-];
-// Definir años a partir de 2024
-const currentYear = new Date().getFullYear();
-const startYear = 2024; // Año inicial deseado
-const years = Array.from({ length: currentYear - startYear + 1 }, (_, i) => startYear + i);
-
-let semanaSeleccionada = 1; // Por defecto, la primera semana
-let mesSeleccionado = 1; // Por defecto, enero
-let anioSeleccionado = currentYear; // Por defecto, el año actual
-
 </script>
 
 <template>
     <PrincipalLayout title="Reportes">
         <div class="mt-1 bg-white p-4 shadow rounded-lg h-5/6">
             <h2 class="font-bold text-center text-xl pt-0 mb-2">Reportes</h2>
-            <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px"></div>
-            <Mensaje />
+            <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mb-2"></div>
 
-            <div class="sm:col-span-2 px-4 mb-4">
-                <h3 class="font-bold text-l pt-0">Buscar por: </h3>
-                <div class="flex flex-wrap gap-4">
-                    <div>
-                        <label for="unidad" class="block text-sm font-medium leading-6 text-gray-900">Unidad</label>
-                        <div class="mt-2">
-                            <select name="unidad" id="unidad" v-model="form.unidad"
-                                class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                <option value="" disabled selected>Seleccione la unidad</option>
-                                <option value="todas">Todas las unidades</option>
-                                <option v-for="carro in unidad" :key="carro.idUnidad" :value="carro.idUnidad">
-                                    {{ carro.numeroUnidad }}
-                                </option>
-                            </select>
-                        </div>
-                    </div>
-                    <div>
-                        <label for="operador" class="block text-sm font-medium leading-6 text-gray-900">Operador</label>
-                        <div class="mt-2">
-                            <select name="operador" id="operador" v-model="form.operador"
-                                class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                <option value="" disabled selected>Seleccione el operador</option>
-                                <option value="todas">Todos los operadores</option>
-                                <option v-for="chofer in operador" :key="chofer.idOperador" :value="chofer.idOperador">
-                                    {{ chofer.nombre_completo }}
-                                </option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div v-for="reporte in reportes" :key="reporte.titulo" class="mb-4 bg-zinc-100 rounded-lg p-4">
-                <h3 class="text-lg font-semibold mb-2">{{ reporte.titulo }}</h3>
-                <div class="flex flex-wrap space-x-3 mb-2">
-                    <select v-model="reporte.periodoSeleccionado"
-                        class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                        <option value="semana">Semanal</option>
-                        <option value="mes">Mensual</option>
-                        <option value="anio">Anual</option>
-                    </select>
-                    <template v-if="reporte.periodoSeleccionado === 'semana'">
-                        <select v-model="semanaSeleccionada"
-                            class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                            <option v-for="(week, index) in weeks" :key="index" :value="week">Semana {{ week }}</option>
-                        </select>
-                    </template>
-                    <template v-else-if="reporte.periodoSeleccionado === 'mes'">
-                        <select v-model="mesSeleccionado"
-                            class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                            <option v-for="(month, index) in months" :key="index" :value="index + 1">{{ month }}
-                            </option>
-                        </select>
-                    </template>
-                    <template v-else-if="reporte.periodoSeleccionado === 'anio'">
-                        <select v-model="anioSeleccionado"
-                            class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                            <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
-                        </select>
-                    </template>
-                </div>
-                <div class="flex flex-wrap space-x-3">
-                    <button v-for="formato in formatos" :key="formato.tipo" :class="formato.clase"
-                        @click="generarArchivo(reporte, formato.tipo, form.unidad, { tipo: reporte.periodoSeleccionado, valor: reporte.periodoSeleccionado === 'semana' ? semanaSeleccionada : reporte.periodoSeleccionado === 'mes' ? mesSeleccionado : reporte.periodoSeleccionado === 'anio' ? anioSeleccionado : '' })">
-                        <i :class="formato.icono + ' mr-2 jump-icon'"></i> {{ formato.texto }}
-                    </button>
-                </div>
-            </div>
+            <ReportesEntrada :unidad="props.unidad" :operador="props.operador"/>
+            
         </div>
     </PrincipalLayout>
 </template>
-
 
 <style>
 .jump-icon:hover i {
