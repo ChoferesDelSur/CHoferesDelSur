ae17579e74479a46687efa5b90ab8d69161a4fc1
Ernesto <ernestoalonsomartinezmartinez@gmail.com>
2024-08-18 10:59:52 -0600

    Cambio en services

--- config/services.php
+++ config/services.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'postmark' => [
+    /* 'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
@@ -22,7 +22,7 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
+    ], */
 
     /* 'slack' => [
         'notifications' => [
@@ -30,11 +30,11 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ], */
-    'mailgun' => [
+    /* 'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
         'scheme' => 'https',
-    ],
+    ], */
 
 ];
