ce745c745f8e3d38df3f4df07f94a4e1b41341c2
Ernesto <ernestoalonsomartinezmartinez@gmail.com>
2024-08-21 15:37:10 -0600

    Hice modificacion en vite

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/',
+    /* base: '/',
     build: {
         outDir: 'public/build',
-    },
+    }, */
     server: {
-        https: true, // Asegúrate de que HTTPS esté habilitado si es necesario
+        //https: true, // Asegúrate de que HTTPS esté habilitado si es necesario
         //port: 5173, // Puedes especificar el puerto que prefieras
-        //host: '192.168.0.13', // Esto permite que el servidor escuche en todas las interfaces de red disponibles
+        host: '192.168.0.13', // Esto permite que el servidor escuche en todas las interfaces de red disponibles
     },
     resolve: {
         alias: {
