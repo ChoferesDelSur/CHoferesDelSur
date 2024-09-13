9ec8aa5a1518d9d5c23eb772dfc5a540db3464f9
Choferes Del Sur <choferesdelsur843@gmail.com>
2024-09-13 07:53:21 -0600

    filtrar en formulario

--- resources/js/Components/Servicio/FormularioRegHoraEntrada.vue
+++ resources/js/Components/Servicio/FormularioRegHoraEntrada.vue
@@ -4,6 +4,8 @@ import { ref, watch } from 'vue';
 import Modal from '../Modal.vue';
 //import { route } from '../../../../vendor/tightenco/ziggy/src/js';
 import { route } from 'ziggy-js';
+import vSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
 import axios from 'axios';
 
 const props = defineProps({
@@ -119,21 +121,18 @@ const save = async () => {
                                 </div>
                             </div>
                         </div>
-                        <div class="sm:col-span-2 px-4">
+                        <div class="sm:col-span-2 w-56 px-4">
                             <label for="unidad" class="block text-sm font-medium leading-6 text-gray-900">Unidad</label>
                             <div class="mt-2">
-                                <select name="unidad" :id="'unidad' + op" v-model="form.unidad"
+                                <v-select
+                                    v-model="form.unidad"
+                                    :options="unidadesConOperador.map(carro => ({ label: carro.numeroUnidad, value: carro.idUnidad }))"
                                     placeholder="Seleccione la unidad"
-                                    class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                    <option value="" disabled selected>Seleccione la unidad</option>
-                                    <option v-for="carro in unidadesConOperador" :key="carro.idUnidad" :value="carro.idUnidad">
-                                        {{ carro.numeroUnidad }}
-
-                                    </option>
-                                </select>
+                                    :reduce="unidad => unidad.value"
+                                    class="w-full">
+                                </v-select>
                             </div>
-                            <div v-if="unidadError != ''" class="text-red-500 text-xs mt-1">{{ unidadError }}
-                            </div>
+                            <div v-if="unidadError != ''" class="text-red-500 text-xs mt-1">{{ unidadError }}</div>
                         </div>
                         <div class="sm:col-span-2 px-4"> <!-- Definir el tamaño del cuadro de texto -->
                             <label for="horaEntrada" class="block text-sm font-medium leading-6 text-gray-900">Hora de
