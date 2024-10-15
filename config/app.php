20992e02896383be11a70cb9c94b404e895b4d93
ChoferesDelSur <choferesdelsur843@gmail.com>
2024-10-15 11:26:54 -0600

    Cambio en la url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://www.servicio.choferesdelsur.com'),
+    'url' => env('APP_URL', 'https://servicio.choferesdelsur.com'),
     
     'asset_url' => env('ASSET_URL'),
 
